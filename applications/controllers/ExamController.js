951387d77146cfa2169888091ebf019b871745a7
hamzahfauzy <hamzahfauzy97@gmail.com>
2020-11-26 20:54:29 +0700

    fix table report

--- applications/controllers/ExamController.js
+++ applications/controllers/ExamController.js
@@ -923,14 +923,13 @@ exports.reportDetail = async (req,res) => {
         delete user.metas.school
         // delete user.sequences
         rows += "<td>"+(i+1)+"</td>"
-        rows += "<td>"+user.name+"</td>"
         rows += "<td>\'"+user.username+"</td>"
+        rows += "<td>"+user.name+"</td>"
         rows += "<td>"+school.name+"</td>"
         rows += "<td>"+user.metas.tempat_tanggal_lahir+"</td>"
         rows += "<td>"+user.metas.jenis_kelamin+"</td>"
         rows += "<td>"+d+"</td>"
-        rows += "<td>"+user.metas.hp+"</td>"
-
+        
         var sequences = user.metas.sequences
         if(typeof sequences === 'undefined')
         {
@@ -950,6 +949,7 @@ exports.reportDetail = async (req,res) => {
         }
         else
         {
+            rows += "<td>"+user.metas.hp+"</td>"
             rows += "<td>"+user.metas.academyc_sma.jurusan+"</td>"
             rows += "<td>"+user.metas.nilai_tertinggi_x.mata_pelajaran+"</td>"
             rows += "<td>"+user.metas.nilai_tertinggi_x.nilai+"</td>"
