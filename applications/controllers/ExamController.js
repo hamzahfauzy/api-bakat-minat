15ef463daca606ae94570b4c28c0ea51efac8e67
hamzahfauzy <hamzahfauzy97@gmail.com>
2020-11-26 19:44:54 +0700

    remove user sequences

--- applications/controllers/ExamController.js
+++ applications/controllers/ExamController.js
@@ -1134,6 +1134,7 @@ exports.reportDetail = async (req,res) => {
         rows += "<td>"+logika_verbal+"</td>"
         rows += "<td>"+logika_angka+"</td>"
         rows += "</tr>"
+        delete user.metas.sequences
     }
 
     var html_response = "<title>LAPORAN MINAT BAKAT "+school.name+"</title>"
